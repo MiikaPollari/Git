d477954d36fd557b5605af475335f6998b35a485
MiikaPollari <98429648+MiikaPollari@users.noreply.github.com>
2022-01-26 12:50:16 +0200

    Second commit, added new protocol, Vehicle

--- TestHub.playground/Contents.swift
+++ TestHub.playground/Contents.swift
@@ -1,3 +1,22 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+protocol Vehicle {
+    var name:String { get }
+    var sound:String { get }
+    func sounds()
+}
+
+extension Vehicle {
+    func sounds() {
+        print("\(name) sounds like this: \"\(sound)\"")
+    }
+}
+struct Rocket: Vehicle {
+    let name = "Rocket"
+    var sound: String = "Woooooooshhhhh"
+}
+
+let rocket = Rocket()
+rocket.sounds()
